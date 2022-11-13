a4ba095df40ea2a5e1ed28def59322e372d5a1df
deshondixon <deshondixon@gmail.com>
2022-11-13 15:32:34 -0800

    final flag fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,6 +44,12 @@ class Footer extends Component {
                 }
                 style={{ display: "inline" }}
               >
+                <span
+                  className="iconify language-icon"
+                  data-icon=""
+                  data-inline="false"
+                  id={window.$secondaryLanguageIconId}
+                ></span>
               </div>
             </div>
             <div className="col">
